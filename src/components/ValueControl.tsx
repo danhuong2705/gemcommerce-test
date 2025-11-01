59f9a9e89ed4edeb42ecaa68e561261f6c6cba9f
emma-chtlab <133728823+emma-chtlab@users.noreply.github.com>
2025-11-01 23:50:32 +0700

    remove unuse import

--- src/components/ValueControl.tsx
+++ src/components/ValueControl.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo } from "react";
 import UnitToggle from "./UnitToggle";
 import ValueStepper from "./ValueStepper";
 
